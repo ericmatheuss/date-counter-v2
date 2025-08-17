7ae6f5897b23e75e8245bd1d332d5711093f0fc1
ericmatheuss <contatoericmatheus@gmail.com>
2025-08-17 17:15:16 -0300

    mudança p/ tornar o nome da function reset mais legivel e adição de uma div envolvendo o button reset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  const handleReset = () => {
+    setStep(1);
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min={1}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        ></input>
+
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>
+          <input
+            type="text"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          ></input>
+        </span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+
+        <p>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>{date.toDateString()}</span>
+        </p>
+
+        <div>
+          {(step !== 1 || count !== 0) && (
+            <button type="reset" onClick={handleReset}>
+              Reset
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
